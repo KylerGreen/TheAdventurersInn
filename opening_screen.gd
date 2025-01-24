de2ba0bed7acecde2339ef1287321fbb30c3aed4
KylerGreen <Kyleredmask@gmail.com>
2025-01-24 15:43:21 -0700

    Got rid of failed code for the begin button

--- opening_screen.gd
+++ opening_screen.gd
@@ -1,9 +1,1 @@
 extends Node2D
-
-
-
-func _on_button_pressed():
-	var camera = get_node("Camera2D")
-	camera.set_position(Vector2(100, 100))
-	var new_scene = load("res://CardSelect.tscn")
-	get_tree().change_scene(new_scene)
