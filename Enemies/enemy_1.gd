59c1293df58944dbc662106347ab977ef1007390
KylerGreen <Kyleredmask@gmail.com>
2025-01-27 16:13:51 -0700

    Updating Combat/working on health bar

--- Enemies/enemy_1.gd
+++ Enemies/enemy_1.gd
@@ -1,4 +1,13 @@
 extends CharacterBody2D
 
+var health = 10
+
 func _ready():
 	%Enemy1_Animation.play("Idle")
+
+func _on_button_pressed():
+	health -= 1
+	if health <= 0:
+		queue_free()
+
+#%EnemyHealth.set_value()
