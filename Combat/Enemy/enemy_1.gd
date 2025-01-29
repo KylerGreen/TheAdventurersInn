403d4ca3fd7502847f7e6dfcb1e14a96aefa8d51
KylerGreen <Kyleredmask@gmail.com>
2025-01-29 16:28:51 -0700

    Added Enemy turn added

--- Combat/Enemy/enemy_1.gd
+++ Combat/Enemy/enemy_1.gd
@@ -6,15 +6,16 @@ func _ready():
 	%Enemy1_Animation.play("Idle")
 	%EnemyHealth.value = health
 
-#func _on_button_pressed():
-	#health -= 2
-	#%EnemyHealth.value = health
-	#if health <= 0:
-		#queue_free()
-
+func Enemy_turn():
+	%Enemy_Attack.start()
 
 func _on_timer_timeout():
 	health -= 2
 	%EnemyHealth.value = health
 	if health <= 0:
 		queue_free()
+	Enemy_turn()
+
+
+func _on_enemy_attack_timeout():
+	%Enemy_Attack.stop()
--- Combat/Player/player.gd
+++ Combat/Player/player.gd
@@ -4,16 +4,26 @@ var health = 10
 
 func _ready():
 	%Player_Animation.play("Idle")
+	%PlayerHealth.value = health
 
 func _on_button_pressed():
 	%Arm_Animation.visible = true
 	%Player_Animation.play("Attack")
 	%Arm_Animation.play("Attack")
-	%Timer.start()
+	%Player_Attack.start()
+	%Attack.visible = false
 
 func _on_timer_timeout():
-	%Timer.stop()
+	%Player_Attack.stop()
 	%Player_Animation.stop()
 	%Arm_Animation.stop()
 	%Arm_Animation.visible = false
 	%Player_Animation.play("Idle")
+
+
+func _on_enemy_attack_timeout():
+	health -= 2
+	%PlayerHealth.value = health
+	if %PlayerHealth.value <= 0:
+		queue_free()
+	%Attack.visible = true
