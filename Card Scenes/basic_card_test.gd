ea959059ba0e1a862b3221a201bbec95d8a6824a
ContraZombie4 <gameking599@gmail.com>
2025-02-05 16:16:31 -0700

    Updated Card Organization

--- Card Scenes/basic_card_test.gd
+++ Card Scenes/basic_card_test.gd
@@ -1,12 +1,20 @@
-extends Node2D
+class_name Card extends Node2D
 
+@export var card_name: String = "Card Name"
+@export var card_ability: String = "Card Ability"
+@export var card_rank: int = 1
+@export var card_image: Node2D
 
-var name_label
+@onready var name_lbl: Label = $CardBackground/Name
+@onready var cr_display: Label = $CRDisplay/CR_Label
+@onready var ability_lbl: Label = $AbilityBox/Ability
+@onready var re_action: Label = $ActionReaction/Action_Or_Reaction
 
 func _ready():
-	name_label = %Name
-	update_text("Punch Good")
+	name_lbl.set_text(card_name)
+	cr_display.set_text(str(card_rank))
+	ability_lbl.set_text(card_ability)
 	
 
-func update_text(new_text):
-	name_label.text = new_text
+#func update_text(new_text):
+	#name_label.text = new_text
