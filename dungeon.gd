2ed368c96b87ac34feca3a5e1eda1ff9f17f54ea
kaitlinheusner <kheusner@byui.edu>
2025-02-07 18:10:22 -0700

    Update dungeon.gd

--- dungeon.gd
+++ dungeon.gd
@@ -1,28 +1,26 @@
 extends Node2D
 
-const SPAWN_ROOMS: Array = [preload("res://Rooms/Beginner Rooms/beginner_room_1_r.tscn"),
-preload("res://Rooms/Beginner Rooms/beginner_room_2_uld.tscn"), 
-preload("res://Rooms/Beginner Rooms/beginner_room_3_u.tscn"), 
-preload("res://Rooms/Beginner Rooms/beginner_room_4_d.tscn"), 
-preload("res://Rooms/Beginner Rooms/beginner_room_5_l.tscn"), 
-preload("res://Rooms/Beginner Rooms/beginner_room_6_lr.tscn"), 
-preload("res://Rooms/Beginner Rooms/beginner_room_7uldr.tscn"), 
-preload("res://Rooms/Beginner Rooms/beginner_room_8_ud.tscn")]
+const SPAWN_ROOMS: Array = [preload("res://Rooms/Beginner Rooms/beginner_room_1_r.tscn")]
+#preload("res://Rooms/Beginner Rooms/beginner_room_2_uld.tscn"), 
+#preload("res://Rooms/Beginner Rooms/beginner_room_3_u.tscn"), 
+#preload("res://Rooms/Beginner Rooms/beginner_room_4_d.tscn"), 
+#preload("res://Rooms/Beginner Rooms/beginner_room_5_l.tscn"), 
+#preload("res://Rooms/Beginner Rooms/beginner_room_6_lr.tscn"), 
+#preload("res://Rooms/Beginner Rooms/beginner_room_7uldr.tscn"), 
+#preload("res://Rooms/Beginner Rooms/beginner_room_8_ud.tscn")]
 
 const INTERMEDIATE_ROOMS: Array = [preload("res://Rooms/Intermediate Rooms/intermediate_room_1.tscn"), 
 preload("res://Rooms/Intermediate Rooms/intermediate_room_2.tscn"), 
 preload("res://Rooms/Intermediate Rooms/intermediate_room_3.tscn"), 
 preload("res://Rooms/Intermediate Rooms/intermediate_room_4.tscn")]
 
-const END_ROOMS: Array = [preload("res://Rooms/Advanced Rooms/advanced_room_1.tscn"), 
+const ADVANCED_ROOMS: Array = [preload("res://Rooms/Advanced Rooms/advanced_room_1.tscn"), 
 preload("res://Rooms/Advanced Rooms/advanced_room_2.tscn"),
 preload("res://Rooms/Advanced Rooms/advanced_room_3.tscn"), 
 preload("res://Rooms/Advanced Rooms/advanced_room_4.tscn")]
 
 @onready var player = %Player3
 @onready var camera = $Camera2D
-var last_room_position := Vector2(0,0)
-var room_offset := Vector2(400, 0)
 var active_rooms := []
 var max_rooms := 5
 
@@ -43,7 +41,7 @@ func _spawn_new_room():
 		room_scene = INTERMEDIATE_ROOMS.pick_random()
 
 	else: 
-		room_scene = END_ROOMS.pick_random()
+		room_scene = ADVANCED_ROOMS.pick_random()
 	
 	var room_instance = room_scene.instantiate()	
 	add_child(room_instance)
