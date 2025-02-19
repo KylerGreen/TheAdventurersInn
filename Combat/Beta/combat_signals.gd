465b6edfd39b35c42dd566fe9d28b218019defb2
KylerGreen <Kyleredmask@gmail.com>
2025-02-19 15:58:35 -0700

    Prepping Signals for later

--- Combat/Beta/combat_signals.gd
+++ Combat/Beta/combat_signals.gd
@@ -1,6 +1,19 @@
 extends Node2D
 
 #Player Signals
-signal Action_Set
-signal Player_Attack
+signal Player_Swing
 signal Player_Bolster
+signal Player_Heal
+signal Player_Disarm
+signal Player_Dodge
+signal Player_Parry
+signal Player_Counter
+
+#Enemy Signals
+signal Enemy_Swing
+signal Enemy_Bolster
+signal Enemy_Heal
+signal Enemy_Disarm
+signal Enemy_Dodge
+signal Enemy_Parry
+signal Enemy_Counter
