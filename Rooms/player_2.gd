61708d4424695d913331efa1163ea58b137bc95d
broomale000 <bro24013@byui.edu>
2025-02-21 13:42:15 -0700

    20x20 rooms (resized)

--- Rooms/player_2.gd
+++ Rooms/player_2.gd
@@ -5,6 +5,5 @@ func _physics_process(delta):
 	velocity = direction * 200
 	move_and_slide()
 
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	print('collided') # Replace with function body.
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print('collided')
