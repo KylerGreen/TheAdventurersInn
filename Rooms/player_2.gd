d010b8dc09800ce64dd98e8134a38e3ee73b7dc5
KylerGreen <Kyleredmask@gmail.com>
2025-03-05 16:16:04 -0700

    Combat Stuff

--- Rooms/player_2.gd
+++ Rooms/player_2.gd
@@ -1,28 +1,28 @@
 extends CharacterBody2D
 
-var combat_screen = preload("res://Combat/Alpha/combat_screen.tscn")
+#var combat_screen = preload("res://Combat/Alpha/combat_screen.tscn")
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 200
 	move_and_slide()
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	var only_once : bool = true
-	if only_once:
-		%Doorway.queue_free()
-		%Area2D.queue_free()
-		%Switch.queue_free()
-		%Switch2.set_visible(true)
-		only_once = false
-	
-func _on_area_2d_2_body_entered(body: Node2D) -> void:
-		get_tree().paused = true
-		var combat = combat_screen.instantiate()
-		get_parent().add_child(combat)
-		combat.process_mode = Node.PROCESS_MODE_ALWAYS
-		get_parent().add_child(combat)
-		combat.position = Vector2(150, 150)
+#func _on_area_2d_body_entered(body: Node2D) -> void:
+	#var only_once : bool = true
+	#if only_once:
+		#%Doorway.queue_free()
+		#%Area2D.queue_free()
+		#%Switch.queue_free()
+		#%Switch2.set_visible(true)
+		#only_once = false
+	#
+#func _on_area_2d_2_body_entered(body: Node2D) -> void:
+		#get_tree().paused = true
+		#var combat = combat_screen.instantiate()
+		#get_parent().add_child(combat)
+		#combat.process_mode = Node.PROCESS_MODE_ALWAYS
+		#get_parent().add_child(combat)
+		#combat.position = Vector2(150, 150)
 		
 		
 		
